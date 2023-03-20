0dd72c8685c5d97bfd192840aefb42503b35458a
PatKo1010 <Pat19951010>
2023-03-21 01:42:12 +0800

    start working on modifying and adding function

--- src/main/java/com/Intumit/bulletinBoard/web/bullet/BulletController.java
+++ src/main/java/com/Intumit/bulletinBoard/web/bullet/BulletController.java
@@ -3,10 +3,14 @@ package com.Intumit.bulletinBoard.web.bullet;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.Intumit.bulletinBoard.web.bullet.service.BulletService;
 import com.Intumit.bulletinBoard.web.user.User;
@@ -43,6 +47,14 @@ public class BulletController {
 		}
 		model.addAttribute("action", "add");
 		return "bulletin-add-edit";
+	}
+	
+	@PostMapping ("bullet/add")
+	public ResponseEntity<Bullet> add (@ModelAttribute Bullet bullet ){
+		Bullet result = bulletService.add(bullet);
+		return new ResponseEntity<>(result , HttpStatus.OK);
+		
+		
 	}
 	
 
