65f65bd77c010f3b7582daa08ec4122427ddbde4
Patrick.Ko <Patrick.Ko@acer.com>
2023-03-20 15:10:08 +0800

    init user and bulletin views

--- src/main/java/com/Intumit/bulletinBoard/web/bullet/BulletController.java
+++ src/main/java/com/Intumit/bulletinBoard/web/bullet/BulletController.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.Intumit.bulletinBoard.web.bullet.service.BulletService;
+import com.Intumit.bulletinBoard.web.user.User;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class BulletController {
@@ -16,11 +19,20 @@ public class BulletController {
 	@Autowired
 	BulletService bulletService;
 	
-	@GetMapping("/bullets/{page}")
-	@ResponseBody
-	public HashMap<String, Object> findAll(@PathVariable("page") Integer page){
+	@GetMapping("/bullets/page/{page}")
+	public String findAll(@PathVariable("page") Integer page, Model model, HttpServletRequest request){
+		User user = (User) (request.getSession().getAttribute("user"));
+		if (user == null) {
+			return "login";
+		}
 		Integer size = 3;
-		return bulletService.findBulletsByPage(page, size);
+		page = page-1;
+		HashMap<String, Object> resultMap = bulletService.findBulletsByPage(page, size);
+		model.addAttribute( "bullets", resultMap.get("bullets"));
+		model.addAttribute( "currentPage", resultMap.get("currentPage"));
+		model.addAttribute( "totalItems", resultMap.get("totalItems"));
+		model.addAttribute( "totalPages", resultMap.get("totalPages"));
+		return "bullets";
 	}
 	
 
--- src/main/java/com/Intumit/bulletinBoard/web/user/UserController.java
+++ src/main/java/com/Intumit/bulletinBoard/web/user/UserController.java
@@ -1,16 +1,18 @@
 package com.Intumit.bulletinBoard.web.user;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.Intumit.bulletinBoard.web.bullet.Bullet;
 import com.Intumit.bulletinBoard.web.user.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -25,17 +27,16 @@ public class UserController {
 	
 	@PostMapping("/logIn")
 	@ResponseBody
-	public User login (@RequestBody User user){
+	public ResponseEntity<User> login (@RequestBody User user, HttpServletRequest request){
 		User result;
 		try {
 			result = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
-//			return new ResponseEntity<>(result, HttpStatus.OK);
-			return result;
+			HttpSession session = request.getSession();
+			session.setAttribute("user", result);
+			return new ResponseEntity<> (result, HttpStatus.OK);
 		} catch (Exception e) {
 			result = null;
-			e.printStackTrace();
-//			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-			return null;
+			return ResponseEntity.notFound().build();
 		}
 	} 
 
